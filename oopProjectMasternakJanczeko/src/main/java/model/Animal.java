3f12f6f02e988ff3406fcc005fef4beaed1315ef
Tomasz Janeczko <tomekj3009@gmail.com>
2024-01-08 00:14:03 +0100

    genes changed from List to ArrayList + small changes

--- oopProjectMasternakJanczeko/src/main/java/model/Animal.java
+++ oopProjectMasternakJanczeko/src/main/java/model/Animal.java
@@ -1,24 +1,23 @@
 package model;
-import java.util.List;
+import java.util.ArrayList;
 
 public class Animal {
     private final static int STARTENERGY = 150;         //as for now
     private final static int NUMBEROFGENES = 3;         //as for now
     private Position position;
-    private final List<Integer> genes;
+    private final ArrayList<Integer> genes;
     private int activeGeneNumber;
     private int energyLeft;
     private WorldDirection facedSide;
-
     private AnimalInformation historian;
 
-    public Animal(Position position, List<Integer> genes) {
+    public Animal(Position position, ArrayList<Integer> genes) {
         this.position = position;
         this.genes = genes;
         this.activeGeneNumber = 0;
         this.energyLeft = STARTENERGY;
         this.facedSide = WorldDirection.values()[(int) (Math.random() * WorldDirection.values().length)];
-        historian = new AnimalInformation (0);
+        this.historian = new AnimalInformation (0);
     }
 
     public Position getPosition() {
@@ -27,6 +26,10 @@ public class Animal {
 
     public void setPosition(Position position) {
         this.position = position;
+    }
+
+    public ArrayList<Integer> getGenes() {
+        return genes;
     }
 
     public Integer getActiveGene() {
@@ -51,12 +54,14 @@ public class Animal {
 
     public void move() {
         facedSide = facedSide.performNextNTimes (getActiveGene());
-        setPosition(getPosition().add(facedSide.toPosition()));         //as for now -> later check possibility to go to that place
+        setPosition(getPosition().add(facedSide.toPosition()));
+        //as for now -> later check chance to go to that place
+        //right and left edges are possible to cross; upper and bottom edges change facedSite to opposite
         activeGeneNumber=(activeGeneNumber+1)%NUMBEROFGENES;
     }
 
     public void eat() {
-        //checking for every plant whether there are animals there:
-        //1 - easy case, 2 and more -> ConflictDealer
+        //checking for every plant whether there are animals there
+        //if eat then -> historian.increaseNumberOfEatenPlants();
     }
 }

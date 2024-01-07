263ba76a52ee5c63f42f8f2e10e1dffa5b40722d
Tomasz Janeczko <tomekj3009@gmail.com>
2024-01-08 00:15:29 +0100

    added 2 new functions

--- oopProjectMasternakJanczeko/src/main/java/model/AnimalInformation.java
+++ oopProjectMasternakJanczeko/src/main/java/model/AnimalInformation.java
@@ -5,22 +5,30 @@ public class AnimalInformation {
     private HashSet<Animal> children;
     private int numberOfEatenPlants;
     private final int birth;
-    private int death = -1; //as long as -1 that means animal is alive
+    private int death = -1;             //as long as -1 that means animal is alive
 
     public AnimalInformation (int birth) {
-        children = new HashSet<Animal>();
+        this.children = new HashSet<Animal>();
         this.birth = birth;
     }
 
     public void addChild(Animal child) {
-        children.add (child);
+        children.add(child);
     }
 
-    public void die (int death) {
+    public void increaseNumberOfEatenPlants() {
+        this.numberOfEatenPlants += 1;
+    }
+
+    public void die(int death) {
         this.death = death;
     }
 
-    public int getNumberOfDescendants() {
+    public int getNumberOfChildren() {
         return children.size();
     }
+
+//    public int getNumberOfDescendants(Animal animal) {
+//
+//    }
 }
